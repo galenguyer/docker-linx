a37c4cb39bb5f601cc69e179ce55424fc72fbb06
seednode <git@seedno.de>
2020-06-25 15:32:59 -0400

    No longer push image to registry

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ basedir="$(pwd)"
 docker build -t docker.seedno.de/seednode/linx:latest "$basedir"/.
 
 # log in to docker registry
-pass show docker-credential-helpers/docker-pass-initialized-check && docker login docker.seedno.de
+#pass show docker-credential-helpers/docker-pass-initialized-check && docker login docker.seedno.de
 
 # push the image to registry
-docker push docker.seedno.de/seednode/linx:latest
+#docker push docker.seedno.de/seednode/linx:latest
